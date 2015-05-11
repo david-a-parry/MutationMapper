b17ef32606c5c8627f28b9376479c8241d9b8a5d
David Parry <gantzgraf@users.noreply.github.com>
2015-05-11 23:01:45 +0100

    remove redundant import

--- src/com/github/mutationmapper/AboutController.java
+++ src/com/github/mutationmapper/AboutController.java
@@ -7,7 +7,6 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
