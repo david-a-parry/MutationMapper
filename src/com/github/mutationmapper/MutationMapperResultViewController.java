b83bfb2dc7b39e20c098cc21461fe6d6e2de12c6
David A. Parry <d.a.parry@leeds.ac.uk>
2015-05-21 18:03:31 +0100

    remove redundant imports

--- src/com/github/mutationmapper/MutationMapperResultViewController.java
+++ src/com/github/mutationmapper/MutationMapperResultViewController.java
@@ -52,7 +52,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckMenuItem;
 import javafx.scene.control.ContextMenu;
