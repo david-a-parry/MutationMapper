c5a7089c3f4ca2fea9ab15b4d9acbaf63cc2f259
David A. Parry <d.a.parry@leeds.ac.uk>
2015-05-21 18:04:55 +0100

    add @Override annotation

--- src/com/github/mutationmapper/SpeciesComparator.java
+++ src/com/github/mutationmapper/SpeciesComparator.java
@@ -29,33 +29,26 @@ import java.util.List;
  * This comparator sorts VEP consequences in order of severity
  */
 public class SpeciesComparator implements Comparator<String>{
-    private static final List<String> CONS_ORDER = Arrays.asList("transcript_ablation", 
-            "splice_acceptor_variant", "splice_donor_variant", "stop_gained", 
-            "frameshift_variant", "stop_lost", "initiator_codon_variant", 
-            "transcript_amplification", "inframe_insertion", "inframe_deletion", 
-            "missense_variant", "splice_region_variant", 
-            "incomplete_terminal_codon_variant", "stop_retained_variant", 
-            "synonymous_variant", "coding_sequence_variant", 
-            "mature_miRNA_variant", "5_prime_UTR_variant", 
-            "3_prime_UTR_variant", "non_coding_transcript_exon_variant", 
-            "intron_variant", "NMD_transcript_variant", 
-            "non_coding_transcript_variant", "upstream_gene_variant", 
-            "downstream_gene_variant", "TFBS_ablation", "TFBS_amplification", 
-            "TF_binding_site_variant", "regulatory_region_ablation", 
-            "regulatory_region_amplification", "regulatory_region_variant", 
-            "feature_elongation", "feature_truncation", "intergenic_variant");
+    private static final List<String> SPECIES_ORDER = Arrays.asList(
+                "Human", "Mouse", "Rat", "Zebrafish", "Fruitfly");
+
     @Override
-    public int compare(String s1, String s2){
-        if (CONS_ORDER.contains(s1)){
-            if (CONS_ORDER.contains(s2)){
-                return CONS_ORDER.indexOf(s1) - CONS_ORDER.indexOf(s2);
-            }else{
-                return -1;
-            }
-        }else if (CONS_ORDER.contains(s2)){
+    public int compare(String s1, String s2) {
+        if (s1 == null){
             return 1;
-        }else{
-            return s1.compareTo(s2);
         }
+        if (s2 == null){
+            return -1;
+        }
+        if (SPECIES_ORDER.contains(s1)){
+            if (SPECIES_ORDER.contains(s2)){
+                return SPECIES_ORDER.indexOf(s1) - SPECIES_ORDER.indexOf(s2);
+            }
+            return -1;
+        }else if (SPECIES_ORDER.contains(s2)){
+            return 1;
+        }
+        return s1.compareTo(s2);
     }
+        
 }
