cd398782756f1c32b774b1ccacb9afed91ef9ed7
David <David@orangebox.home>
2015-07-21 12:14:24 +0100

    Ensembl seem to have changed some 'is_canonical' values to longs now. Either way, get String value and parse as int.

--- src/com/github/mutationmapper/EnsemblRest.java
+++ src/com/github/mutationmapper/EnsemblRest.java
@@ -180,18 +180,11 @@ public class EnsemblRest {
         String biotype = (String)j.get("biotype");
         trans.setBiotype(biotype);
         if (j.containsKey("is_canonical")){
-            if (j.get("is_canonical") instanceof String){
-                if (Integer.parseInt((String)j.get("is_canonical")) > 0){
-                    trans.setIsCanonical(true);
-                }else{
-                    trans.setIsCanonical(false);
-                }
+            String isCanon = j.get("is_canonical").toString();
+            if (Integer.parseInt(isCanon) > 0){
+                trans.setIsCanonical(true);
             }else{
-                if ((Long) j.get("is_canonical") > 0){
-                    trans.setIsCanonical(true);
-                }else{
-                    trans.setIsCanonical(false);
-                }
+                trans.setIsCanonical(false);
             }
         }
         if (j.containsKey("strand")){
