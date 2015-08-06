982dde6691d1d6b524a6becd30e48e37bba4064f
David A. Parry <david.parry@igmm.ed.ac.uk>
2015-08-06 13:43:48 +0100

    remove unused import

--- src/com/github/mutationmapper/MutationMapper.java
+++ src/com/github/mutationmapper/MutationMapper.java
@@ -29,7 +29,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
