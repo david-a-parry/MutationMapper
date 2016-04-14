9070f4d6ff951e4d5f6ee01bd41a6f149a76a0ef
gantzgraf <d.a.parry@leeds.ac.uk>
2016-04-14 23:34:48 +0100

    add extra plugins

--- src/com/github/mutationmapper/EnsemblRest.java
+++ src/com/github/mutationmapper/EnsemblRest.java
@@ -474,7 +474,9 @@ public class EnsemblRest {
             String species, String ref, String alt) throws ParseException, 
             MalformedURLException, IOException, InterruptedException {
         HashMap<String, HashMap<String, String>> results = new HashMap<>();
-        String endpoint = "/vep/" + species +"/region?hgvs=1;canonical=1;numbers=1;xref_refseq=1";
+        String endpoint = "/vep/" + species +"/region?hgvs=1;canonical=1;"
+                + "numbers=1;xref_refseq=1;miRNA=1;domains=1;ExAC=1;Blosum62=1;"
+                + "MaxEntScan=1;GeneSplicer=1";
         String post = String.format("{ \"variants\" : [\"%s  %d  . %s %s . . .\" ] }", 
                 chrom, coord, ref, alt);
         JSONArray info = (JSONArray) getPostJSON(endpoint, post);
